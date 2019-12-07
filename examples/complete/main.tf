a1aba2080ca9f5e7b02ffb551e2fd980fc6e3f86
aknysh <andriy.knysh@gmail.com>
2019-12-07 00:54:12 -0500

    Initial implementation. Add tests. Add Codefresh test pipeline

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -36,7 +36,7 @@ module "subnets" {
   vpc_id               = module.vpc.vpc_id
   igw_id               = module.vpc.igw_id
   cidr_block           = module.vpc.vpc_cidr_block
-  nat_gateway_enabled  = false
+  nat_gateway_enabled  = true
   nat_instance_enabled = false
   tags                 = local.tags
 }
