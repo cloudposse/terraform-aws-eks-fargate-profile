55e658ec1c1273175bc93f1ec39785fb6530d393
aknysh <andriy.knysh@gmail.com>
2020-08-18 15:26:39 -0400

    Update test

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -58,6 +58,18 @@ module "eks_cluster" {
   workers_security_group_ids = []
 }
 
+# Ensure ordering of resource creation to eliminate the race conditions when applying the Kubernetes Auth ConfigMap.
+# Do not create Node Group before the EKS cluster is created and the `aws-auth` Kubernetes ConfigMap is applied.
+# Otherwise, EKS will create the ConfigMap first and add the managed node role ARNs to it,
+# and the kubernetes provider will throw an error that the ConfigMap already exists (because it can't update the map, only create it).
+# If we create the ConfigMap first (to add additional roles/users/accounts), EKS will just update it by adding the managed node role ARNs.
+data "null_data_source" "wait_for_cluster_and_kubernetes_configmap" {
+  inputs = {
+    cluster_name             = module.eks_cluster.eks_cluster_id
+    kubernetes_config_map_id = module.eks_cluster.kubernetes_config_map_id
+  }
+}
+
 module "eks_node_group" {
   source             = "git::https://github.com/cloudposse/terraform-aws-eks-node-group.git?ref=tags/0.6.0"
   namespace          = var.namespace
@@ -70,7 +82,7 @@ module "eks_node_group" {
   desired_size       = var.desired_size
   min_size           = var.min_size
   max_size           = var.max_size
-  cluster_name       = module.eks_cluster.eks_cluster_id
+  cluster_name       = data.null_data_source.wait_for_cluster_and_kubernetes_configmap.outputs["cluster_name"]
   kubernetes_version = var.kubernetes_version
   kubernetes_labels  = var.kubernetes_labels
 }
@@ -83,7 +95,7 @@ module "eks_fargate_profile" {
   attributes           = var.attributes
   tags                 = var.tags
   subnet_ids           = module.subnets.private_subnet_ids
-  cluster_name         = module.eks_cluster.eks_cluster_id
+  cluster_name         = data.null_data_source.wait_for_cluster_and_kubernetes_configmap.outputs["cluster_name"]
   kubernetes_namespace = var.kubernetes_namespace
   kubernetes_labels    = var.kubernetes_labels
 }
